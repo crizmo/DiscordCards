f46ee422c879f90c1e4ce47d27f266bc99fc4567
crizmo <crizmo595@gmail.com>
2022-06-11 17:35:32 +0530

    Update app.js

--- server-dcez/app.js
+++ server-dcez/app.js
@@ -23,7 +23,7 @@ app.get('/new', (req, res) => {
 app.get('/card', (req, res) => {
     const member = client.guilds.cache.get('939799133177384990').members.cache.get('784141856426033233');
     const activity = member.presence.activities[0];
-    console.log(activity);
+    // console.log(activity);
     res.send(activity);
 })
 
@@ -32,23 +32,25 @@ app.get('/card/:id', (req, res) => {
     const id = req.params.id;
     const member = client.guilds.cache.get('939799133177384990').members.cache.get(id);
     const activity = member.presence.activities[0];
-    console.log(activity);
+    // console.log(activity);
     res.send(activity);
 })
 
-// client.on("presenceUpdate", function (oldPresence, newPresence, args, oldActivity, newActivity) {
+client.on("presenceUpdate", function (oldPresence, newPresence, args, oldActivity, newActivity) {
 
-//     const currentActivity = newPresence.activities[0];
+    const currentActivity = newPresence.activities[0];
 
-//     if (newPresence.user.id === "784141856426033233" && newPresence.user.bot === false) {
-//         console.log(`${newPresence.user.tag} is ${newPresence.status}`);
-//         if (currentActivity.name === "Spotify") {
-//             console.log(`${newPresence.user.username} is listening to ${currentActivity.details} \nby ${currentActivity.state}}`);
-//         } else if (currentActivity.name === "Code") {
-//             console.log(`${newPresence.user.username} ${currentActivity.details} \n${currentActivity.state}`);
-//         }
-//     }
-// });
+    if (newPresence.user.id === "784141856426033233" && newPresence.user.bot === false) {
+        // console.log(`${newPresence.user.tag} is ${newPresence.status}`);
+        if (currentActivity.name === "Spotify") {
+            console.log(`${newPresence.user.username} is listening to ${currentActivity.details} \nby ${currentActivity.state}}`);
+        } else if (currentActivity.name === "Code") {
+            console.log(`${currentActivity.state}`);
+        }   
+    }
+
+});
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 client.login(process.env.DISCORD_TOKEN);
