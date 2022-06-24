8b4d6e1dce172e547913d5916d52bbcc8893df55
crizmo <crizmo595@gmail.com>
2022-06-24 21:49:16 +0530

    Update app.js

--- server-dcez/app.js
+++ server-dcez/app.js
@@ -17,7 +17,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://breeze-stats.netlify.app/',
         methods: ["GET", "POST"],
     }
 })
