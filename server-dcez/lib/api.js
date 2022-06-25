0bd5824197d2ef0541f1801e01b5a3b907ebf580
crizmo <crizmo595@gmail.com>
2022-06-26 00:25:13 +0530

    Update api.js

--- server-dcez/lib/api.js
+++ server-dcez/lib/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const fs = require('fs');
+const imageToBase64 = require('image-to-base64');
 
 const { Client, Intents, Collection } = require('discord.js');
 const Discord = require('discord.js');
@@ -20,32 +21,88 @@ const api = () => {
             small_image: 'req.query.small_image',
         } */
 
+        /* base64 images
+            pfp (discord_avatar),
+            banner,
+            spotify_logo,
+            large_image,
+            small_image,
+            play_along,
+            side image
+        */
+
         let member
         try {
             member = await client.guilds.cache.get("782646778347388959").members.fetch(req.params.id);
+            if (member.user.bot) {
+                res.send("User is a bot")
+                return
+            }
         } catch (e) {
             res.send('User not found ! Please try again')
             // console.log(e)
             return;
         }
-        
-        let activity, discord_avatar, spotify_logo, username, banner, about, play_along
+
+        let username , discord_avatar, banner, about
+        let activity, name, type, details, state
+        let large_image, small_image, side_image
+        let smallimg, raw // for PLaying
+        let spotify_logo, play_along, largeText // for Spotify
+
+        let pfp64, banner64
+        let spotify64, play_along64
+        let large64, small64, side64
 
         try {
             discord_avatar = member.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })
-            spotify_logo = 'https://www.freeiconspng.com/uploads/spotify-icon-0.png'
             username = member.user.username + '#' + member.user.discriminator
             banner = req.query.banner || 'https://cdn.discordapp.com/attachments/970974282681307187/987323350709862420/green-back.png'
             about = req.query.about || ' '
             if (about.length > 20) {
                 about = about.substring(0, 20) + "..."
             }
-
+            
+            spotify_logo = 'https://www.freeiconspng.com/uploads/spotify-icon-0.png'
             play_along = "https://cdn.discordapp.com/attachments/970974282681307187/987330240609132555/play-along.png"
+
+            large_image = req.query.large_image || discord_avatar
+            small_image = req.query.small_image || discord_avatar
+            side_image = req.query.side_image || discord_avatar
+
         } catch (e) {
             res.send('Uh user error')
             console.log(e)
             return;
+        }
+
+        try {
+            pfp64 = await imageToBase64(discord_avatar)
+            pfp64 = `data:image/png;base64,${pfp64}`
+
+            banner64 = await imageToBase64(banner)
+            banner64 = `data:image/png;base64,${banner64}`
+
+            spotify64 = await imageToBase64(spotify_logo)
+            spotify64 = `data:image/png;base64,${spotify64}`
+
+            play_along64 = await imageToBase64(play_along)
+            play_along64 = `data:image/png;base64,${play_along64}`
+
+            large64 = large_image
+            large64 = await imageToBase64(large64)
+            large64 = `data:image/png;base64,${large64}`
+            
+            small64 = small_image
+            small64 = await imageToBase64(small64)
+            small64 = `data:image/png;base64,${small64}`
+    
+            side64 = side_image
+            side64 = await imageToBase64(side64)
+            side64 = `data:image/png;base64,${side64}`
+
+        } catch (e) {
+            console.log(e)
         }
         
         try {
@@ -65,7 +122,6 @@ const api = () => {
         } catch (e) {
             let temp;
             function no_activity() {
-                let type, details
                 try {
                     type = req.query.type || 'Chilling'
                     details = req.query.details || 'Vibiing'
@@ -82,19 +138,18 @@ const api = () => {
                 }
 
                 temp = fs.readFileSync('./assets/cards/no-activity-new.svg', {encoding: 'utf-8'}).toString()
-                temp = temp.replace('[pfp]', discord_avatar);
+                temp = temp.replace('[pfp]', pfp64);
                 temp = temp.replace('[username]', username);
-                temp = temp.replace('[banner]', banner);
+                temp = temp.replace('[banner]', banner64);
                 temp = temp.replace('[about]', about);
 
                 temp = temp.replace('[type]', type);
                 temp = temp.replace('[details]', details);
 
-                temp = temp.replace('[large-image]', req.query.large_image || discord_avatar)
-                temp = temp.replace('[small-image]', req.query.small_image || discord_avatar)
-                temp = temp.replace('[side-image]', req.query.side_image || discord_avatar)
+                temp = temp.replace('[large-image]', large64);
+                temp = temp.replace('[small-image]', small64);
+                temp = temp.replace('[side-image]', side64);
 
-                console.log(`${member.user.username} has no activity`)
                 // res.send('User has no activity')
                 res.writeHead(200, {'Content-Type': 'image/svg+xml'})
                 res.end(temp)
@@ -102,8 +157,6 @@ const api = () => {
             no_activity()
             return;
         }
-
-        let name, details, state, smallimg, raw, largeText
             
         if(!activity.name){
             name = 'No name'
@@ -133,15 +186,17 @@ const api = () => {
         }
         
         if(!activity.assets){
-            raw = req.query.large_image || discord_avatar
+            raw = pfp64
         } else if(activity.assets.smallImage === null) {
-            raw = req.query.large_image || discord_avatar
+            raw = pfp64
         } else if(activity.assets.smallImage.startsWith('mp:external')){
             smallimg = activity.assets.smallImage
             let smalllink = smallimg.split('https/')[1]
             raw = 'https://' + smalllink
+            raw = await imageToBase64(raw)
+            raw = `data:image/png;base64,${raw}`
         } else {
-            raw = req.query.large_image || discord_avatar   
+            raw = pfp64  
         }
 
         if(!activity.assets){
@@ -154,10 +209,18 @@ const api = () => {
                 largeText = largeText.substring(0, 23) + '...';
             }
         }
+
+        large64 = req.query.large_image || discord_avatar
+        large64 = await imageToBase64(large64)
+        large64 = `data:image/png;base64,${large64}`
+        
+        small64 = req.query.small_image || discord_avatar
+        small64 = await imageToBase64(small64)
+        small64 = `data:image/png;base64,${small64}`
         
         let temp;
         if (activity.name === 'Spotify') {
-
+            
             let start = activity.timestamps.start
             let end = activity.timestamps.end
             let elapsed = end - start
@@ -167,20 +230,21 @@ const api = () => {
 
             temp = fs.readFileSync('./assets/cards/spotify-new.svg', {encoding: 'utf-8'}).toString()
             temp = temp.replace('[username]', username);
-            temp = temp.replace('[banner]', banner);
+            temp = temp.replace('[pfp]', pfp64);
+            temp = temp.replace('[banner]', banner64);
             temp = temp.replace('[about]', about);
-
-            temp = temp.replace('[play-along]', play_along);
-
+            
             temp = temp.replace('[details]', details);
             temp = temp.replace('[state]', state);
             temp = temp.replace('[type]', req.query.type || activity.type);
             temp = temp.replace('[on]', largeText);
             temp = temp.replace('[time]', 0 + ' -- ' + timeString);
-            temp = temp.replace('[pfp]', discord_avatar);
-            temp = temp.replace('[large-image]', req.query.large_image || discord_avatar);
-            temp = temp.replace('[small-image]', req.query.small_image || spotify_logo);
-            temp = temp.replace('[spotify-logo]', spotify_logo);
+            
+            temp = temp.replace('[large-image]', large64);
+            temp = temp.replace('[small-image]', small64);
+
+            temp = temp.replace('[spotify-logo]', spotify64);
+            temp = temp.replace('[play-along]', play_along64);
             temp = temp.replace('[button-text]', "Play on Spotify");
         } else if (activity.name === 'Code') {
 
@@ -195,23 +259,30 @@ const api = () => {
             let largelink = largeimage.split('raw')[1]
             const rawlarge = 'https://raw' + largelink
 
+            large64 = await imageToBase64(rawlarge)
+            large64 = `data:image/png;base64,${large64}`
+
             const smallimage = activity.assets.smallImage
             let smallink = smallimage.split('raw')[1]
             const rawsmall = 'https://raw' + smallink
 
+            small64 = await imageToBase64(rawsmall)
+            small64 = `data:image/png;base64,${small64}`
+
             temp = fs.readFileSync('./assets/cards/vscode-new.svg', {encoding: 'utf-8'}).toString()
             temp = temp.replace('[username]', username);
-            temp = temp.replace('[banner]', banner);
+            temp = temp.replace('[banner]', banner64);
             temp = temp.replace('[about]', about);
-            temp = temp.replace('[pfp]', discord_avatar);
+            temp = temp.replace('[pfp]', pfp64);
 
             temp = temp.replace('[name]', activity.name || 'Gaming');
             temp = temp.replace('[details]', activity.details || 'No details');
             temp = temp.replace('[state]', state || 'No description');
             temp = temp.replace('[type]', req.query.type || activity.type);
             temp = temp.replace('[time]', timeString + ' elapsed' || '0:00 elapsed');
-            temp = temp.replace('[large-image]', rawlarge);
-            temp = temp.replace('[small-image]', rawsmall);
+
+            temp = temp.replace('[large-image]', large64);
+            temp = temp.replace('[small-image]', small64);
             temp = temp.replace('[button-text]', activity.buttons[0] || 'Playing');
         } else if (activity.type === 'PLAYING') {
 
@@ -232,16 +303,17 @@ const api = () => {
 
             temp = fs.readFileSync('./assets/cards/game-new.svg', {encoding: 'utf-8'}).toString()
             temp = temp.replace('[username]', username);
-            temp = temp.replace('[banner]', banner);
+            temp = temp.replace('[banner]', banner64);
             temp = temp.replace('[about]', about);
-            temp = temp.replace('[pfp]', discord_avatar);
+            temp = temp.replace('[pfp]', pfp64);
 
             temp = temp.replace('[name]', name || 'Gaming');
             temp = temp.replace('[details]', details || 'No details');
             temp = temp.replace('[state]', state || 'No description');
             temp = temp.replace('[type]', req.query.type || activity.type);
-            temp = temp.replace('[large-image]', raw);
             temp = temp.replace('[time]', timeString + ' elapsed' || '0:00 elapsed');
+
+            temp = temp.replace('[large-image]', raw);
         }
         res.writeHead(200, {'Content-Type': 'image/svg+xml'})
         res.end(temp)
